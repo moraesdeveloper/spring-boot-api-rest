bd2dfc39df6638b29b7d4d0f01fa315887cb5e8b
moraesdeveloper <joaopedro_moraes97@outlook.com>
2019-09-12 22:56:10 -0300

    segundo commit

--- src/main/java/com/system/apirest/service/ClienteService.java
+++ src/main/java/com/system/apirest/service/ClienteService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.system.apirest.exceptions.ResourceNotFoundException;
 import com.system.apirest.model.Cliente;
 import com.system.apirest.repository.ClienteRepository;
 
@@ -21,7 +22,10 @@ public class ClienteService {
 	
 	public Cliente buscarPorCodigo(Long codigo) {
 		Cliente cliente = clienteRepository.findByCodigo(codigo);
-		return clienteRepository.save(cliente);
+		if(cliente == null) {
+			throw new ResourceNotFoundException("Não foi encontrado o cliente : " +codigo);
+		}
+		return cliente;
 	}
 	
 	public Cliente salvar(Cliente cliente) {
@@ -30,9 +34,22 @@ public class ClienteService {
 	
 	public Cliente atualizar(Cliente cliente, Long codigo) {
 		Cliente entity = clienteRepository.findByCodigo(codigo);
+		if(entity == null) {
+			throw new ResourceNotFoundException("Não foi encontrado o cliente : " +codigo);
+
+		}
 		BeanUtils.copyProperties(cliente, entity, "codigo");
 		clienteRepository.save(entity);
 		return entity;
 	}
+	
+	public void deletar(Long codigo) {
+		Cliente cliente = clienteRepository.findByCodigo(codigo);
+		if(cliente == null) {
+			throw new ResourceNotFoundException("Não foi encontrado o cliente : " +codigo);
+		}
+		clienteRepository.delete(cliente);
+	}
+	
 	
 }
