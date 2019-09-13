d4a12abc86731288d35843bf3587cb959bb6d4d8
moraesdeveloper <joaopedro_moraes97@outlook.com>
2019-09-13 14:01:38 -0300

    validando disponibilidade

--- src/main/java/com/system/apirest/resource/AgendamentoResource.java
+++ src/main/java/com/system/apirest/resource/AgendamentoResource.java
@@ -2,11 +2,15 @@ package com.system.apirest.resource;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.system.apirest.model.Agendamento;
@@ -29,7 +33,8 @@ public class AgendamentoResource {
 	
 	
 	@PostMapping
-	public Agendamento salvar(@RequestBody Agendamento agendamento) {
+	@ResponseStatus(HttpStatus.CREATED)
+	public Agendamento salvar(@Valid @RequestBody Agendamento agendamento) {
 		return agendamentoService.salvar(agendamento);
 	}
 	
--- src/main/java/com/system/apirest/service/AgendamentoService.java
+++ src/main/java/com/system/apirest/service/AgendamentoService.java
@@ -1,8 +1,11 @@
 package com.system.apirest.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.system.apirest.exceptions.ResourceNotFoundException;
 import com.system.apirest.model.Agendamento;
 import com.system.apirest.repository.AgendamentoRepository;
 
@@ -14,7 +17,21 @@ public class AgendamentoService {
 	
 	
 	public Agendamento salvar(Agendamento agendamento) {
+		validaDisponibilidade(agendamento);
 		return agendamentoRepository.save(agendamento);
 	}
+	
+	public void validaDisponibilidade(Agendamento agendamento) {
+		List<Agendamento> todos = agendamentoRepository.findAll();
+		for(Agendamento agendamentos : todos) {
+			if(agendamentos.getData().equals(agendamento.getData())
+					&& agendamentos.getHorario().equals(agendamento.getHorario())) {
+				throw new ResourceNotFoundException("Horário indisponivel para realizar agendamento");
+			}
+		}
+	}
+	
+	
+	
 	
 }
