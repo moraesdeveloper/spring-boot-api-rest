e2735fb6c7c1e29104f70eb80f92b499f9f03838
moraesdeveloper <joaopedro_moraes97@outlook.com>
2019-09-12 23:00:48 -0300

    terceiro commit

--- src/main/java/com/system/apirest/resource/ClienteResource.java
+++ src/main/java/com/system/apirest/resource/ClienteResource.java
@@ -3,12 +3,15 @@ package com.system.apirest.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.system.apirest.model.Cliente;
@@ -32,6 +35,7 @@ public class ClienteResource {
 	}
 	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente salvar(@RequestBody Cliente cliente) {
 		return clienteService.salvar(cliente);
 	}
@@ -39,5 +43,11 @@ public class ClienteResource {
 	@PutMapping("/{codigo}")
 	public Cliente atualizar(@RequestBody Cliente cliente, @PathVariable Long codigo) {
 		return clienteService.atualizar(cliente, codigo);
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Long codigo) {
+		clienteService.deletar(codigo);
 	}
 }
